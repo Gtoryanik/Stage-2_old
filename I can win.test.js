f6a9e462f3be8b1627b69be698192520fbec1684
Gtoryanik <102662879+Gtoryanik@users.noreply.github.com>
2022-06-09 15:29:14 +0400

    Update I can win.test.js

--- I can win.test.js
+++ I can win.test.js
@@ -1,19 +1,28 @@
 describe ('I can win', ()=>{
-    it ('should work',async ()=>{
+    it ('should work', async ()=>{
         await browser.url ('https://pastebin.com/');
         const paste = 'Hello from WebDriver';
-        const pasteInput = $('//textarea[@name="PostForm[text]"]');
-        const btn = $('#w0 > div.post-form__bottom > div.post-form__left > div.form-group.form-btn-container > button');
         const pasteName = 'helloweb';
-        const pasteNameInput = $('#postform-name');
-        const finalPaste = $('body > div.wrap > div.container > div.content > div.post-view.js-post-view > div.highlighted-code > div.source > ol > li > div')
-        const finPasteName = $('body > div.wrap > div.container > div.content > div.post-view.js-post-view > div.details > div.info-bar > div.info-top > h1')
-        await pasteInput.addValue(paste);
-        await pasteNameInput.addValue(pasteName);
-        await expect(btn).toBeClickable();
-        await btn.click ();
-        await expect(finalPaste).toHaveText(paste);
-        await expect(finPasteName).toHaveText(pasteName);
-        await browser.pause(3000);
-    });
+        const pasteinput = $('#postform-text')
+        const pasteNameInput = $('#postform-name')
+        const button = $('div.form-group.form-btn-container > button');
+        await pasteinput.addValue(paste);
+        await pasteNameInput.addValue(pasteName); 
+
+        const arrow = await $('#select2-postform-expiration-container');
+        await arrow.isClickable();
+        await arrow.click ();
+        const list = await $('#select2-postform-expiration-results');
+        const option = await list.$$('li')[2];
+        await option.isClickable();
+        await option.click();
+        await button.click();
+
+        const pasteNameResult = $('div.info-top > h1');
+        const pasteResult = $('div.source > ol > li > div');
+        const pasteExpirationResult = $('div.expire');
+        await expect(pasteNameResult).toHaveTextContaining('helloweb');
+        await expect(pasteResult).toHaveTextContaining('Hello from WebDriver');
+        await expect(pasteExpirationResult).toHaveTextContaining('10 MIN');
+        })Ж
 })
